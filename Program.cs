bb23a9f82ee8c93ca0b7c8642479519da9d7f694
sukreshmanda <mandasukresh@gmail.com>
2023-09-24 14:29:13 +0530

    Hello world program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyNameSpace
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+        }
+    }
+    class Game
+    {
+        public Game()
+        {
+            Console.WriteLine("Game started");
+        }
+        ~Game()
+        {
+            Console.WriteLine("Game finished");
+        }
+    }
+}
